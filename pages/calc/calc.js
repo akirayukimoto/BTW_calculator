789469fba8c0695fba076fbf6a16d83757177adc
Lingbin <sunlingbinangela@gmail.com>
2019-08-21 16:53:49 -0400

    增加选择关卡选项
    谁来帮忙弄一下属性值整理啊啊啊啊——【终于要开始搭算数了大概】

--- pages/calc/calc.js
+++ pages/calc/calc.js
@@ -1,20 +1,32 @@
 var cards = [
   {
     id: 0,
-    card_name: "Fake Love SUGA",
+    card_name: ''
   },
   {
     id: 1,
-    card_name: 'Fake Love J-Hope',
+    card_name: "Fake Love SUGA",
   },
   {
     id: 2,
-    card_name: 'Fake Love RM',
+    card_name: 'Fake Love J-Hope',
   },
   {
     id: 3,
+    card_name: 'Fake Love RM',
+  },
+  {
+    id: 4,
     card_name: 'Fake Love Jung Kook',
+  },
+  {
+    id: 5,
+    card_name: '决战时刻'
   }
+]
+
+var stages = [
+  '', '6-1', '6-2', '6-4', '6-5', '6-7', '6-8', '6-9', '6-11', '6-12', '6-14', '6-15', '6-17', '6-18', '6-19'
 ]
 
 Page({
@@ -32,39 +44,55 @@ Page({
     cards_2: JSON.parse(JSON.stringify(cards)),
     cards_3: JSON.parse(JSON.stringify(cards)),
     cards_4: JSON.parse(JSON.stringify(cards)),
+    stage_index: 0,
+    cur_stages: JSON.parse(JSON.stringify(stages))
   },
 
   /**
-   * bindPicker: 选择你想要的卡
+   * stagePicker: 选择你正在面对的关卡
+   */
+  stagePicker: function(e) {
+    console.log('选择的关卡是：', this.data.cur_stages[e.detail.value])
+    this.setData({
+      stage_index: e.detail.value
+    })
+  },
+
+  /**
+   * bindPicker: 选择你想要用来计算的卡
    */
 
   bindPicker1: function(e) {
-    console.log('picker 1选择名称变为', e.detail.value)
+    console.log('picker 1选择名称变为', this.data.cards_1[e.detail.value].card_name)
     this.setData({
       index_1: e.detail.value
     })
   },
 
   bindPicker2: function (e) {
-    console.log('picker 2选择名称变为', e.detail.value)
+    console.log('picker 2选择名称变为', this.data.cards_2[e.detail.value].card_name)
     this.setData({
       index_2: e.detail.value
     })
   },
 
   bindPicker3: function (e) {
-    console.log('picker 3选择名称变为', e.detail.value)
+    console.log('picker 3选择名称变为', this.data.cards_3[e.detail.value].card_name)
     this.setData({
       index_3: e.detail.value
     })
   },
 
   bindPicker4: function (e) {
-    console.log('picker 4选择名称变为', e.detail.value)
+    console.log('picker 4选择名称变为', this.data.cards_4[e.detail.value].card_name)
     this.setData({
       index_4: e.detail.value
     })
   },
+
+  /**
+   * 
+   */
 
   /**
    * 生命周期函数--监听页面加载
