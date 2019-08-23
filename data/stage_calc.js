89efa66eb484058db47850312524c869009fe452
Lingbin <sunlingbinangela@gmail.com>
2019-08-23 14:16:59 -0400

    更新：没填的地方都comment掉，然后公式清单整理好了
    大概，你帮忙debug一下

--- data/stage_calc.js
+++ data/stage_calc.js
@@ -1,6 +1,8 @@
 var stage_percent = [
   {
+    // 第一章：共计三个公式
     "id": 1,
+    "card_num": 1,
     "property_1": 1.8,
     "property_2": 1.0,
     "property_3": 1.0,
@@ -8,34 +10,60 @@ var stage_percent = [
   },
   {
     "id": 2,
+    "card_num": 1,
+    "property_1": 1.8,
+    "property_2": 1.5,
+    "property_3": 1.0,
+    "property_4": 1.0
+  },
+  {
+    "id": 3,
+    "card_num": 2,
+    "property_1": 1.8,
+    "property_2": 1.5,
+    "property_3": 1.0,
+    "property_4": 1.0
+  },
+  {
+    // 第二章
+    "id": 4,
+    "card_num": 2,
     "property_1": 1.8,
     "property_2": 1.5,
     "property_3": 1.0,
     "property_4": 0.5
   },
   {
-    "id": 3,
+    // 第三章
+    "id": 5,
+    "card_num": 3,
     "property_1": 1.8,
     "property_2": 1.5,
     "property_3": 0.8,
     "property_4": 0.5
   },
   {
-    "id": 4,
-    "property_1": 2.0,
-    "property_2": 1.5,
-    "property_3": 0.7,
-    "property_4": 0.3
-  },
-  {
-    "id": 5,
-    "property_1": 2.0,
-    "property_2": 1.5,
-    "property_3": 0.7,
-    "property_4": 0.3
-  },
-  {
+    // 第四章
     "id": 6,
+    "card_num": 3,
+    "property_1": 2.0,
+    "property_2": 1.5,
+    "property_3": 0.7,
+    "property_4": 0.3
+  },
+  {
+    // 第五章
+    "id": 7,
+    "card_num": 4,
+    "property_1": 2.0,
+    "property_2": 1.5,
+    "property_3": 0.7,
+    "property_4": 0.3
+  },
+  {
+    // 第六章
+    "id": 8,
+    "card_num": 4,
     "property_1": 2.0,
     "property_2": 1.5,
     "property_3": 0.5,
