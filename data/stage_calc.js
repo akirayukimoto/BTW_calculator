2894f25fdf737bee9c3941e588275c361f25fc21
Lingbin <sunlingbinangela@gmail.com>
2019-08-22 23:27:49 -0400

    更新：乱加了一堆，还有加了算stage的array

---
+++ data/stage_calc.js
@@ -0,0 +1,49 @@
+var stage_percent = [
+  {
+    "id": 1,
+    "percent_1": 1.8,
+    "percent_2": 1.0,
+    "percent_3": 1.0,
+    "percent_4": 1.0
+  },
+  {
+    "id": 2,
+    "percent_1": 1.8,
+    "percent_2": 1.5,
+    "percent_3": 1.0,
+    "percent_4": 0.5
+  },
+  {
+    "id": 3,
+    "percent_1": 1.8,
+    "percent_2": 1.5,
+    "percent_3": 0.8,
+    "percent_4": 0.5
+  },
+  {
+    "id": 4,
+    "percent_1": 2.0,
+    "percent_2": 1.5,
+    "percent_3": 0.7,
+    "percent_4": 0.3
+  },
+  {
+    "id": 5,
+    "percent_1": 2.0,
+    "percent_2": 1.5,
+    "percent_3": 0.7,
+    "percent_4": 0.3
+  },
+  {
+    "id": 6,
+    "percent_1": 2.0,
+    "percent_2": 1.5,
+    "percent_3": 0.5,
+    "percent_4": 0.5
+  }
+
+]
+
+module.exports = {
+  stage_percent: stage_percent
+}
